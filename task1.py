d55195b5144456b14cfdc1a02cfe3d5220bc104d
Zakirov Rifkat <zakirov2323rifkat@gmail.com>
2023-09-29 14:39:24 +0300

    add: main func

--- task1.py
+++ task1.py
@@ -1,18 +1,34 @@
 import numpy as np
 import scipy.linalg as sp
 
+
 def spectralRadius(matrix):
     eigenvalues = sp.eigvals(matrix)
     return max(abs(eigenvalues))
 
-def q(x,y):
+
+def q(x, y):
     return 1
 
-def p(x,y):
-    return 1+2*x
 
-if __name__ == '__main__':
-    print(5)    
+def p(x, y):
+    return 1 + 2 * x
 
 
+def f(x, y):
+    return (
+        2 * y**2
+        + 2 * y**3
+        + 8 * x * y**2
+        + 8 * x * y**3
+        + 2 * x**2
+        + 6 * x**2 * y
+    )
 
+
+def mu(x, y):
+    return x**2 * y**2 + x**2 * y**3
+
+
+if __name__ == "__main__":
+    print(f(0, np.pi))
