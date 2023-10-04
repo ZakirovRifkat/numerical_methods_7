02ac4e09c0b0cf1726bd448946741b09c35b278c
Zakirov Rifkat <zakirov2323rifkat@gmail.com>
2023-10-04 13:22:13 +0300

    done

--- task1.py
+++ task1.py
@@ -1,12 +1,17 @@
 import numpy as np
+from tabulate import tabulate
 
 EPS = 0.001
 
 np.set_printoptions(formatter={"all": lambda x: f"{x:.4f}"})  # Set the format
 
 
-def spectralRadius(xi):
+def spectralRadiusApprox(xi):
     return (1 - xi) / (1 + xi)
+
+
+def itterationApprox(ksi):
+    return np.log(1 / EPS) / (2 * ksi)
 
 
 def q(x, y):
@@ -59,7 +64,7 @@ def fillBorderMatrix(x_i, y_i):
     return matrix
 
 
-def find_max_abs_element(matrix):
+def normOfMatrix(matrix):
     max_abs_element = None
 
     for i in range(1, len(matrix) - 1):
@@ -71,21 +76,42 @@ def find_max_abs_element(matrix):
     return max_abs_element
 
 
-def simpleItteration(x, y, step_x, step_y):
+def exactSolition(x_i, y_i):
+    exact = np.zeros((len(x_i), len(y_i)))
+    for i in range(len(x_i)):
+        for j in range(len(y_i)):
+            exact[i][j] = mu(x_i[i], y_i[j])
+    return exact
+
+
+def f_matrix(x_i, y_i):
+    matrix = np.zeros((len(x_i), len(y_i)))
+    for i in range(len(x_i)):
+        for j in range(len(y_i)):
+            matrix[i][j] = f(x_i[i], y_i[j])
+    return matrix
+
+
+def calculate_lhu(u, x, y, hx, hy):
+    lhu = np.zeros((len(x), len(y)))
+    for i in range(1, len(x) - 1):
+        for j in range(1, len(y) - 1):
+            term1 = p(x[i] + hx / 2, y[j]) * (u[i + 1][j] - u[i][j]) / hx**2
+            term2 = p(x[i] - hx / 2, y[j]) * (u[i][j] - u[i - 1][j]) / hx**2
+            term3 = q(x[i], y[j] + hy / 2) * (u[i][j + 1] - u[i][j]) / hy**2
+            term4 = q(x[i], y[j] - hy / 2) * (u[i][j] - u[i][j - 1]) / hy**2
+            lhu[i][j] = term1 - term2 + term3 - term4
+    return lhu
+
+
+def simpleItteration(x, y, step_x, step_y, iter):
     k = 0
     previous_U = fillBorderMatrix(x, y)
     current_U = fillBorderMatrix(x, y)
-    exact_U = np.zeros((len(x), len(y)))
-    for i in range(len(x)):
-        for j in range(len(y)):
-            exact_U[i][j] = mu(x[i], y[j])
-
     U_0 = np.copy(previous_U)
+    arrayOfU_K = [U_0]
 
-    while (
-        find_max_abs_element(current_U - exact_U) / find_max_abs_element(U_0 - exact_U)
-        > EPS
-    ):
+    while k < iter:
         for i in range(1, len(x) - 1):
             for j in range(1, len(y) - 1):
                 a = (p(x[i] - step_x / 2, y[j]) * previous_U[i - 1][j]) / step_x**2
@@ -102,7 +128,8 @@ def simpleItteration(x, y, step_x, step_y):
         k += 1
 
         previous_U = np.copy(current_U)
-    return current_U, k
+        arrayOfU_K.append(np.copy(current_U))
+    return arrayOfU_K, k
 
 
 x_0 = 0
@@ -122,6 +149,64 @@ d1 = 1
 d2 = 1
 
 ksi = xi(c1, c2, d1, d2, step_x, step_y)
-print("spectr = ", spectralRadius(ksi))
-simpleItterationMatrix, amountItter = simpleItteration(x_i, y_i, step_x, step_y)
-print(amountItter)
+
+f_matrix = f_matrix(x_i, y_i)
+exact_solve = exactSolition(x_i, y_i)
+U_0 = fillBorderMatrix(x_i, y_i)
+print("\nМетод простых иттераций. Вариант 8\n")
+print(f"N = {N}; M = {M}\neps = {EPS}\n")
+print(
+    f"Мера аппроксимации ||F-AU_*|| = {normOfMatrix(calculate_lhu(exact_solve, x_i, y_i, step_x, step_y) +f_matrix)}"
+)
+null_approx = normOfMatrix(calculate_lhu(U_0, x_i, y_i, step_x, step_y) + f_matrix)
+print(f"Норма невязки нулевого приближения ||F-AU_0|| = {null_approx}")
+print(f"Число иттераций = {itterationApprox(ksi)}")
+spectr = spectralRadiusApprox(ksi)
+print(f"Спектральный радиус pho(H)= {spectr}")
+
+array_simple_itteration_matrix, amountItter = simpleItteration(
+    x_i, y_i, step_x, step_y, itterationApprox(ksi)
+)
+approx_solition_table = tabulate(
+    array_simple_itteration_matrix[-1], tablefmt="fancy_grid"
+)
+
+
+exact_solution_table = tabulate(exact_solve, tablefmt="fancy_grid")
+
+
+data = []
+headers = [
+    "k",
+    "||F-AU_k||",
+    "rel.d",
+    "||U^k - U_*||",
+    "rel.error",
+    "||U^k - U^(k-1)||",
+    "apost.est",
+    "sp.red._k",
+]
+
+for k, solve in enumerate(array_simple_itteration_matrix):
+    data.append(
+        [
+            k,
+            normOfMatrix(calculate_lhu(solve, x_i, y_i, step_x, step_y) + f_matrix),
+            normOfMatrix(calculate_lhu(solve, x_i, y_i, step_x, step_y) + f_matrix)
+            / null_approx,
+            normOfMatrix(solve - exact_solve),
+            normOfMatrix(solve - exact_solve) / normOfMatrix(U_0 - exact_solve),
+            normOfMatrix(solve - array_simple_itteration_matrix[k - 1]),
+            (spectr * normOfMatrix(solve - array_simple_itteration_matrix[k - 1]))
+            / (1 - spectr),
+            normOfMatrix(solve - array_simple_itteration_matrix[k-1])/normOfMatrix(array_simple_itteration_matrix[k-1]-array_simple_itteration_matrix[k-2]),
+        ]
+    )
+
+# Выведите таблицу
+table1 = tabulate(data, headers, tablefmt="grid")
+
+
+print(table1)
+print(f"\nПриближенное решение:\n{approx_solition_table}")
+print(f"\nТочное решение:\n{exact_solution_table}")
