1aa2fdbe2fb45e8c655f15448d44ee3b7281e5f3
Zakirov Rifkat <zakirov2323rifkat@gmail.com>
2023-10-25 09:39:56 +0300

    ????

--- task8.py
+++ task8.py
@@ -5,6 +5,30 @@ np.set_printoptions(formatter={"all": lambda x: f"{x:.4f}"})  # Set the format
 EPS = 0.001
 
 
+def tridiagonal_matrix_algorithm(a, b, c, d):
+    n = len(d)
+    c_new = np.zeros(n - 1)
+    d_new = np.zeros(n)
+
+    # Прямой ход (прогонка вперед)
+    c_new[0] = c[0] / b[0]
+    d_new[0] = d[0] / b[0]
+
+    for i in range(1, n - 1):
+        temp = b[i] - a[i] * c_new[i - 1]
+        c_new[i] = c[i] / temp
+        d_new[i] = (d[i] - a[i] * d_new[i - 1]) / temp
+
+    # Обратный ход (прогонка назад)
+    x = np.zeros(n)
+    x[-1] = d_new[-1]
+
+    for i in range(n - 2, -1, -1):
+        x[i] = d_new[i] - c_new[i] * x[i + 1]
+
+    return x
+
+
 def q(x, y):
     return 1
 
@@ -72,6 +96,80 @@ def f_matrix(x_i, y_i):
     return matrix
 
 
+def terms_1(x, y, j, u, tau, hx, hy):
+    term1 = []
+    term2 = []
+    term3 = []
+    term4 = []
+    for i in range(len(x)):
+        term1.append((-1 * p(x[i] - hx / 2, y[j])) / (hx**2))
+        term2.append(
+            (
+                2 / tau
+                + p(x[i] + hx / 2, y[j]) / hx**2
+                + p(x[i] - hx / 2, y[j]) / hx**2
+            )
+        )
+        term3.append((-1 * p(x[i] + hx / 2, y[j])) / (hx**2))
+        g = (
+            (2 * u[i][j]) / tau
+            + (q(x[i], y[j] + hy / 2) * (u[i][j + 1] - u[i][j])) / hy**2
+            - (q(x[i], y[j] - hy / 2) * (u[i][j] - u[i][j - 1])) / hy**2
+            + f(x[i], y[i])
+        )
+        term4.append(g)
+    return term1, term2, term3, term4
+
+
+def terms_2(x, y, j, u, tau, hx, hy):
+    term1 = []
+    term2 = []
+    term3 = []
+    term4 = []
+    for i in range(len(x)):
+        term1.append((-1 * q(x[i], y[j] - hy / 2)) / hy**2)
+        term2.append(
+            (
+                2 / tau
+                + q(x[i], y[j] + hy / 2) / hy**2
+                + q(x[i], y[j] - hy / 2) / hy**2
+            )
+        )
+        term3.append((-1 * p(x[i], y[j] + hy / 2)) / (hy**2))
+        g = (
+            (2 * u[i][j]) / tau
+            + (p(x[i] + hx / 2, y[j]) * (u[i + 1][j] - u[i][j])) / hx**2
+            - (p(x[i] - hx / 2, y[j]) * (u[i][j] - u[i - 1][j])) / hx**2
+            + f(x[i], y[i])
+        )
+        term4.append(g)
+    return term1, term2, term3, term4
+
+
+def alternatingDirectionMethod(x, y, hx, hy, tau):
+    k = 0
+    previous_U = fillBorderMatrix(x, y)
+    current_U = fillBorderMatrix(x, y)
+    U_0 = np.copy(previous_U)
+    arrayOfU_K = [U_0]
+    exact = exactSolition(x, y)
+    # while (normOfMatrix(current_U - exact) / normOfMatrix(U_0 - exact) > EPS): #k<32
+    #     print(1)
+    u_half = np.zeros((len(y), len(x)))
+    for i in range(1, len(x) - 1):
+        # if j == 0:
+        #     for i in range(len(x)):
+        #         u_half[j][i] = mu(x[i], 0)
+        # elif j == len(y):
+        #     for i in range(len(x)):
+        #         u_half[j][i] = mu(x[i], np.pi)
+        # else:
+        term1, term2, term3, term4 = terms_1(x, y, i, previous_U, tau, hx, hy)
+        u_half[j] = tridiagonal_matrix_algorithm(term1, term2, term3, term4)
+    print(u_half)
+    return 0
+
+
 x_0 = 0
 x_n = 1
 
@@ -91,3 +189,15 @@ c1 = 1
 c2 = 3
 d1 = 1
 d2 = 1
+
+sigma_1 = c1 * (4 / (step_x**2)) * ((np.sin((np.pi * step_x) / 2 * 1)) ** 2)
+sigma_2 = c2 * (4 / step_x**2) * ((np.cos((np.pi * step_x) / 2 * 1)) ** 2)
+
+delta_1 = c2 * (4 / (step_y**2)) * ((np.sin((np.pi * step_y) / (2 * np.pi))) ** 2)
+delta_2 = c2 * (4 / (step_y**2)) * ((np.cos((np.pi * step_y) / (2 * np.pi))) ** 2)
+
+sigma = max([sigma_1, sigma_2])
+delta = max([delta_1, delta_2])
+
+tauOpt = 2 / (np.sqrt(sigma * delta))
+alternatingDirectionMethod(x_i, y_i, step_x, step_y, tauOpt)
